709e0c291e980d36a4fdbd1dbedbcd29937c0886
Thomas Baradel <tbaradel@TBaradel.local>
2014-06-16 14:10:47 +0100

    lar

---
+++ laravel/app/models/Comment.php
@@ -0,0 +1,10 @@
+
+<?php
+
+// app/models/Comment.php
+
+class Comment extends Eloquent {
+    // let eloquent know that these attributes will be available for mass assignment
+    protected $fillable = array('author', 'text');
+}
+
